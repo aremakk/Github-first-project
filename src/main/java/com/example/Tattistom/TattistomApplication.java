67d1706d1acad632ab25b77fd99f4accf1d16e9d
ssslowl <ssslowl2005@gmail.com>
2024-02-20 10:00:15 +0600

    first project

---
+++ src/main/java/com/example/Tattistom/TattistomApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tattistom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattistomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattistomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tattistom/TattistomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tattistom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattistomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
